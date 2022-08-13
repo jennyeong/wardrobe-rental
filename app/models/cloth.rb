930b84ee9f483f49a5541b6e68bac306ee70ff2b
Ye Zhu <dellazhuye@gmail.com>
2022-08-13 15:29:13 +0800

    add_belongs_to_in_cloth

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -1,4 +1,5 @@
 class Cloth < ApplicationRecord
+  belongs_to :user
   has_many_attached :photos
   validates :title, :description, :price, :category, :color, :brand, :start_date, :end_date, presence: true
 end
