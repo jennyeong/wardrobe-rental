3c1886d763674ddeea42809717debe56bdc43795
Li Yang Cheng <chengliyang@gmail.com>
2022-08-14 15:22:15 +0800

    Add size to cloths and some css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_13_083742) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_14_044921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_083742) do
     t.datetime "updated_at", null: false
     t.integer "price"
     t.bigint "user_id", null: false
+    t.string "size"
     t.index ["user_id"], name: "index_cloths_on_user_id"
   end
 
