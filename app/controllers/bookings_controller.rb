bd111035324fb10e522bdc0370db7b198804cf0b
Ye Zhu <dellazhuye@gmail.com>
2022-08-14 19:45:28 +0800

    working on booking controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,56 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: %i[show edit update destroy]
+  def index
+    # @cloths = Cloth.all
+    @cloths = policy_scope(Cloth)
+  end
+
+  def show
+    authorize @cloth
+  end
+
+  def new
+    @cloth = Cloth.new
+    authorize @cloth
+  end
+
+  def create
+    @cloth = Cloth.new(cloth_params)
+    @cloth.user = current_user
+    authorize @cloth
+    if @cloth.save
+      redirect_to @cloth, notice: "Cloth was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    authorize @cloth
+  end
+
+  def update
+    authorize @cloth
+    if @cloth.update(cloth_params)
+      redirect_to @cloth, notice: "Cloth was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize @cloth
+    @cloth.destroy
+    redirect_to cloths_path, notice: "Restaurant was successfully destroyed."
+  end
+
+  private
+
+  def set_cloth
+    @cloth = Cloth.find(params[:id])
+  end
+
+  def cloth_params
+    params.require(:cloth).permit(:title, :description, :price, :category, :color, :brand, :start_date, :end_date, photos: [])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :cloths
+  resources :cloths do
+    resources :bookings, only: [:new, :create, :edit, :update]
+  end
+  resources :bookings, only: [:destroyed]
 end
