5f7afe22c7d2c855fa3d13f04d44a43606d45b02
Chee Jenn Yeong <cheejennyeong95@gmail.com>
2022-08-17 00:20:18 +0800

    added reviews except for destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: %i[show edit update destroy]
+  before_action :set_booking, only: %i[new create edit update destroy]
 
   def new
     @review = Review.new
@@ -8,23 +8,43 @@ class ReviewsController < ApplicationController
 
   def create
     @review = Review.new(review_params)
-    @review.booking = params[:booking_id]
+  @review.booking = @booking
     authorize @review
     if @review.save
-      redirect_to @review, notice: "Review was successfully created."
+      redirect_to cloth_path(@booking.cloth), notice: "Thank you for your review."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
+    @review = @booking.review
     authorize @review
+  end
+
+  def update
+    @review = @booking.review
+    authorize @review
+    if @review.update(review_params)
+      redirect_to cloth_path(@booking.cloth), notice: "Thank you for your review."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # @review = @booking.review
+    @review = Review.find(params[:id])
+    authorize @review
+    @review.destroy
+    raise
+    redirect_to cloth_path(@booking.cloth), status: :see_other
   end
 
   private
 
-  def set_review
-    @review = Review.find(params[:id])
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
   end
 
   def review_params
