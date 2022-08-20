24e694e95aa039eecbfd48474fc70433ecd8f183
Ye Zhu <dellazhuye@gmail.com>
2022-08-21 01:49:18 +0800

    fix the bug showing multiple no review yet when no review

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -1,6 +1,7 @@
 class Cloth < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many_attached :photos
   validates :title, :description, :price, :category, :size, :color, :photos, :start_date, :end_date, presence: true
