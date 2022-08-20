b03cee19cef51e4d54906e1a498ec18d6f693af0
Ye Zhu <dellazhuye@gmail.com>
2022-08-21 03:04:47 +0800

    fix the bug of top 5 liked clothes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,16 @@ class PagesController < ApplicationController
     # For Drops
     @cloths = Cloth.where(rented: false).last(5)
     # For Wishlisted
-    # @cloths_wishlisted = Cloth.where(rented: false, brand: "Louis Vuitton").limit(3)
-    @cloths_wishlisted = []
-    Bookmark.all.each do |bookmark|
-      @cloths_wishlisted << bookmark.cloth if bookmark.cloth.rented == false
-    end
-    @cloths_wishlisted = @cloths_wishlisted.first(5)
+    # @cloths_wishlisted = []
+    # Bookmark.all.each do |bookmark|
+    #   @cloths_wishlisted << bookmark.cloth if bookmark.cloth.rented == false
+    # end
+    # @cloths_wishlisted = @cloths_wishlisted.first(5)
+    @cloths_wishlisted = Cloth.select('cloths.*, COUNT(bookmarks.user_id) AS bookmark_count')
+                              .joins(:bookmarks)
+                              .group('cloths.id')
+                              .order('bookmark_count DESC')
+                              .limit(5)
   end
 
   def contact_us
