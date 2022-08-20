03fd7833b005a0d041c398ea6fbc6ee31c75d2b9
Ye Zhu <dellazhuye@gmail.com>
2022-08-20 14:17:37 +0800

    repush

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -3,6 +3,5 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.element.textContent = "Hello World!"
-    console.log("1234567")
   }
 }
